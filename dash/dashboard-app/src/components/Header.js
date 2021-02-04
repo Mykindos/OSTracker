cb30b799cfa3e5841fe2fd321604b8f96a852795
Tom <tomhoogstra@live.com.au>
2021-02-05 02:28:48 +1100

    Remove header

--- dash/dashboard-app/src/components/Header.js
+++ dash/dashboard-app/src/components/Header.js
@@ -7,14 +7,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
 const Header = () => <AppBar position="static">
-    <Toolbar variant="dense">
-      <IconButton edge="start" color="inherit" aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" color="inherit">
-        My Dashboard
-      </Typography>
-    </Toolbar>
-  </AppBar>;
+
+</AppBar>;
 
 export default withRouter(Header);
